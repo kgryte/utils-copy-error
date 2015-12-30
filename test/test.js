3e064cb53e1fabc552dec9d1a2ed4bbe73746b79
kgryte <kgryte@gmail.com>
2015-12-29 18:16:28 -0800

    Use t.plan() rather than t.end() due to https://github.com/substack/tape/issues/223

--- test/test.js
+++ test/test.js
@@ -9,6 +9,6 @@ var copy = require( './../lib' );
 // TESTS //
 
 test( 'main export is a function', function test( t ) {
+	t.plan( 1 );
 	t.ok( typeof copy === 'function', 'export is a function' );
-	t.end();
 });
